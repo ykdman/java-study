83fd0f117cdc7399b15b9530843b2b8777a563e3
YKDman <dbsrudejr300@gmail.com>
2025-05-21 23:31:14 +0900

    의존성 주입 - field 인젝션 - Setter 인젝션 - Constructor 인젝션

---
+++ learn-spring-framwork-02/src/main/java/com/kdspring/learn_spring_framwork/examples/a1/DepInjectionLauncherApplication.java
@@ -0,0 +1,24 @@
+package com.kdspring.learn_spring_framwork.examples.a1;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+@ComponentScan
+public class DepInjectionLauncherApplication {
+
+
+    public static void main(String[] args) {
+        try (var context = new AnnotationConfigApplicationContext
+                (DepInjectionLauncherApplication.class)) {
+            Arrays.stream(context.getBeanDefinitionNames())
+                    .forEach(System.out::println);
+
+        }
+
+
+    }
+}
