c1b76b4570b3462cb63cc1f1dd58e6a948c319a6
YKDman <dbsrudejr300@gmail.com>
2025-05-28 00:00:59 +0900

    spring-boot : /courser API 제작

---
+++ learn-spring-boot/src/main/java/com/ykdman/springboot/learn_spring_boot/CourseController.java
@@ -0,0 +1,23 @@
+package com.ykdman.springboot.learn_spring_boot;
+
+// Course: id, name, author
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    @RequestMapping("/courses")
+    public List<Course> retrieveAllCourses() {
+        return Arrays.asList(
+                new Course(1, "Learn AWS", "in28Minutes"),
+                new Course(2, "Learn DevOps", "in28Minutes")
+        );
+    }
+}
