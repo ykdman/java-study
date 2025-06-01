214598a3ec12710059769c5ce435bf0fa664cc80
YKDman <dbsrudejr300@gmail.com>
2025-06-02 00:01:51 +0900

    spring-boot - devtools 이용 코드 자동변경 적용 - 환경변수 파일 세팅 방법 학습

--- learn-spring-boot/src/main/java/com/ykdman/springboot/learn_spring_boot/CourseController.java
+++ learn-spring-boot/src/main/java/com/ykdman/springboot/learn_spring_boot/CourseController.java
@@ -17,7 +17,9 @@ public class CourseController {
     public List<Course> retrieveAllCourses() {
         return Arrays.asList(
                 new Course(1, "Learn AWS", "in28Minutes"),
-                new Course(2, "Learn DevOps", "in28Minutes")
+                new Course(2, "Learn DevOps", "in28Minutes"),
+                new Course(3, "Learn Azure", "Azure Kdman"),
+                new Course(4, "Learn GCP", "in28minutes")
         );
     }
 }
