d8c64e03e64c1585de71288ee402e3849df42d77
YKDman <dbsrudejr300@gmail.com>
2025-06-29 23:54:01 +0900

    Entity 사용

--- learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learn_jpa_and_hibernate/course/Course.java
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learn_jpa_and_hibernate/course/Course.java
@@ -1,9 +1,19 @@
 package com.in28minutes.springboot.learn_jpa_and_hibernate.course;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
+@Entity
 public class Course {
+
+    @Id
     private long id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "author")
     private String author;
 
     public Course(long id, String name, String author) {
